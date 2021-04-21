288517b6e9ea042e5c4568a52e6935e1d43ac598
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2021-04-21 11:23:58 +0530

    added bottom bar to the main activity

---
+++ app/src/main/java/divyansh/tech/bhagwad_geeta/BhagwadGeeta.kt
@@ -0,0 +1,9 @@
+package divyansh.tech.bhagwad_geeta
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BhagwadGeeta: Application() {
+    override fun onCreate() = super.onCreate()
+}
--- app/src/main/java/divyansh/tech/bhagwad_geeta/presentation/MainActivity.kt
+++ app/src/main/java/divyansh/tech/bhagwad_geeta/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package divyansh.tech.bhagwad_geeta
+package divyansh.tech.bhagwad_geeta.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import divyansh.tech.bhagwad_geeta.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
