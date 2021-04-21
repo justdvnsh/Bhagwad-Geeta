c71474c90eff10b4981c38874654185fc125b002
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2021-04-21 09:37:06 +0530

    initial commit

---
+++ app/src/main/java/divyansh/tech/bhagwad_geeta/MainActivity.kt
@@ -0,0 +1,11 @@
+package divyansh.tech.bhagwad_geeta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
